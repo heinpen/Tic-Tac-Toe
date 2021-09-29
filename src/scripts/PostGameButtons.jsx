ce4d05ab8743cf39ce4b75af67a7cf6779129a5f
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-09-30 00:33:19 +0300

    Add arrow dissabling feature

--- src/scripts/PostGameButtons.jsx
+++ src/scripts/PostGameButtons.jsx
@@ -1,10 +1,20 @@
 import Arrows from './Arrows.jsx';
 
 function PostGameButtons(props) {
-  const { onClickArrows, onClickRestartBtn, onClickNewGameBtn } = props;
+  const {
+    onClickArrows,
+    onClickRestartBtn,
+    onClickNewGameBtn,
+    rightArrowDisabled,
+    leftArrowDisabled,
+  } = props;
   return (
     <>
-      <Arrows onClickArrows={onClickArrows} />
+      <Arrows
+        onClickArrows={onClickArrows}
+        rightArrowDisabled={rightArrowDisabled}
+        leftArrowDisabled={leftArrowDisabled}
+      />
       <button type="button" className="game__post-game-button" onClick={() => onClickRestartBtn()}>
         Restart game
       </button>
